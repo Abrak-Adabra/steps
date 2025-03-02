ae9f54b2e78a33654e49905d4e2103682f2f7bf0
Abrak-Adabra <abrak.adabra@internet.ru>
2025-03-03 00:37:24 +0300

    clear project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    devIndicators: false,
+    output: 'export',
+    basePath: '/steps',
+    images: {
+        unoptimized: true,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
