639b24f2f6f6a745ee60f429ad6ea8b689353df7
Abrak-Adabra <abrak.adabra@internet.ru>
2025-03-08 13:30:50 +0300

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="ru">
             <head>
                 <title>Ступени мастерства</title>
-                <link rel="manifest" href="steps/app.webmanifest" />
-                <link rel="apple-touch-icon" href="steps/ios.png" />
-                <link rel="icon" href="steps/favicon.ico" />
+                <link rel="manifest" href="app.webmanifest" />
+                <link rel="apple-touch-icon" href="ios.png" />
+                <link rel="icon" href="favicon.ico" />
             </head>
             <body>{children}</body>
         </html>
